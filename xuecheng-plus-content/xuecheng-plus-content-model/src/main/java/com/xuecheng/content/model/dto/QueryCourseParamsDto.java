4bb23f0e61284920ca6a3dd589829bfc8ca324da
ArrowInKnees <shendongyaoaero@gmail.com>
2023-08-16 16:03:06 +0800

    update

--- xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
@@ -1,7 +1,9 @@
 package com.xuecheng.content.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
+
 
 /**
  * @ClassName QueryCourseParamsDto
@@ -13,9 +15,12 @@ import lombok.ToString;
 @ToString
 public class QueryCourseParamsDto {
     //审核状态
+    @ApiModelProperty("审核状态")
     private String auditStatus;
     //课程名称
+    @ApiModelProperty("课程名称")
     private String courseName;
     //发布状态
+    @ApiModelProperty("发布状态")
     private String publishStatus;
 }
