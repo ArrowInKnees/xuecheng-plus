9ef9945b201102cbe2e6680dd7234347f1c9e746
ArrowInKnees <shendongyaoaero@gmail.com>
2023-08-17 11:07:51 +0800

    在content-service中添加mapper接口

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/CourseAuditMapper.java
@@ -0,0 +1,17 @@
+package com.xuecheng.content.mapper;
+
+import com.xuecheng.content.model.po.CourseAudit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface CourseAuditMapper extends BaseMapper<CourseAudit> {
+
+}
