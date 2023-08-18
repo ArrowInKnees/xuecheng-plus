a32eefe75a26c4d380c9c6af0292ef76adb3cf9f
ArrowInKnees <shendongyaoaero@gmail.com>
2023-08-18 11:45:35 +0800

    update

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -1,20 +1,13 @@
 package com.xuecheng.content.api;
 
-import com.xuecheng.base.model.PageParams;
-import com.xuecheng.base.model.PageResult;
-import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+
+import com.xuecheng.base.model.*;
+import com.xuecheng.content.model.dto.*;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @ClassName CourseBaseInfoController
@@ -36,4 +29,11 @@ public class CourseBaseInfoController {
         return courseBasePageResult;
     }
 
+    @ApiOperation("新增课程基础信息")
+    @PostMapping("/course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto){
+        //  机构id，由于认证系统没有上线暂时硬编码
+        Long companyId = 1232141425L;
+        return courseBaseInfoService.createCourseBase(companyId,addCourseDto);
+    }
 }
